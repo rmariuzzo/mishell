1f6ef0b0e42930008d5822b05f86595fd4fb6b3e
rmariuzzo <rmariuzzo@users.noreply.github.com>
2021-01-14 17:30:06 +0000

    Build assets.

---
+++ dist/commands/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.commands = void 0;
+var run_if_file_exist_1 = require("./run-if-file-exist");
+exports.commands = [{ name: 'run-if-file-exists', alias: 'rife', run: run_if_file_exist_1.runIfFileExist }];
